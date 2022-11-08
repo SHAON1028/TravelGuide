e46a45ee87024cb618e66c34107bb198f5fc8ff4
SHAON1028 <shaonkhairul@gmail.com>
2022-11-08 18:47:07 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Pages/shared/Footer/Footer';
+import Navbar from './components/Pages/shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
    <Footer></Footer>
     </div>
   );
