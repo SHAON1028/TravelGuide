b6800385da8d46bf920e266de860e4da47039d0a
SHAON1028 <shaonkhairul@gmail.com>
2022-11-08 19:07:18 +0600

    401 403 page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Pages/shared/Footer/Footer';
 import Navbar from './components/Pages/shared/Navbar/Navbar';
+import ErrorPage from './components/Error/ErrorPage';
+import Error401 from './components/Error/Error401';
+import Error403 from './components/Error/Error403';
 
 function App() {
   return (
     <div className="App">
+
       <Navbar></Navbar>
+      <ErrorPage></ErrorPage>
+      <Error401></Error401>
+      <Error403></Error403>
    <Footer></Footer>
+
     </div>
   );
 }
